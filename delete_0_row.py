2819cc31418762bf961c45fcb5e05ac569c9ce60
hinapyon <68768047+hinapyon@users.noreply.github.com>
2023-09-11 14:30:30 +0900

    add delete_o_row.py

---
+++ delete_0_row.py
@@ -0,0 +1,28 @@
+import os
+import pandas as pd
+
+# ディレクトリ内のCSVファイルを取得
+directory_path = 'my_walk_data(100Hz15minutes)'  # ディレクトリのパスを指定してください
+output_directory = 'new_data'  # 出力ディレクトリのパスを指定してください
+
+# ディレクトリ内のCSVファイルをリストアップ
+csv_files = [file for file in os.listdir(directory_path) if file.endswith('.csv')]
+
+# 各CSVファイルに対して処理を実行
+for csv_file in csv_files:
+    # CSVファイルのフルパス
+    file_path = os.path.join(directory_path, csv_file)
+
+    # CSVファイルを読み込み
+    df = pd.read_csv(file_path)
+
+    # 0列目を削除
+    df = df.iloc[:, 1:]
+
+    # 出力ファイルのパス
+    output_file_path = os.path.join(output_directory, csv_file)
+
+    # 新しいCSVファイルに保存
+    df.to_csv(output_file_path, index=False)
+
+print("処理が完了しました。")
